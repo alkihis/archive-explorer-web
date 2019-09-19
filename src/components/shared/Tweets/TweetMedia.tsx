3810df642d68e8233317782c373eb0582e397715
Alkihis <alkihis@gmail.com>
2019-09-19 21:12:38 +0200

    Fix bug for invalid variant when query string

--- src/components/shared/Tweets/TweetMedia.tsx
+++ src/components/shared/Tweets/TweetMedia.tsx
@@ -30,7 +30,7 @@ export default class TweetMedia extends React.Component<TweetMediaProp, TweetMed
   renderVideo(type: "gif" | "mp4", full = false) {
     const m = this.media[0] as MediaGDPREntity;
     // Remove the possible query string
-    m.video_info.variants.forEach(e => e.url.split('?', 2)[0]);
+    m.video_info.variants.forEach(e => e.url = e.url.split('?', 2)[0]);
 
     const valids = m.video_info.variants.filter(e => e.bitrate && e.url.endsWith('.mp4'));
 
