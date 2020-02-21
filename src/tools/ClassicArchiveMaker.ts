28e3fc21c275fbf6669d9ea4a3118db5deffc48c
Louis Béranger <alkihis@gmail.com>
2020-02-21 09:15:48 +0100

    As it

--- src/tools/ClassicArchiveMaker.ts
+++ src/tools/ClassicArchiveMaker.ts
@@ -1,17 +1,12 @@
 import APIHELPER, { API_URLS } from "./ApiHelper";
 import SETTINGS from "./Settings";
 import JSZip from 'jszip';
+import TwitterArchive from "twitter-archive-reader";
 
 const ARCHIVE_URL = API_URLS.classic_archive;
 
 export default new class ClassicArchiveMaker {
-  async make() {
-    if (!SETTINGS.archive) {
-      throw new Error("You need to load an archive first.");
-    }
-
-    const full_archive = SETTINGS.archive;
-
+  async make(full_archive: TwitterArchive) {
     const archive_as_ab: ArrayBuffer = await APIHELPER.request(ARCHIVE_URL, { mode: 'arraybuffer', method: 'GET' });
 
     const created = await JSZip.loadAsync(archive_as_ab);
